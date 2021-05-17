87fecfb3d14acde9cf5f439d6f29850a547d74ce
Nadav Shaar <nadavshaar@gmail.com>
2021-05-17 19:32:16 +0300

    ESlint & Prettier code formatting.

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import { CellContainer } from './';
+import React from "react";
+import { CellContainer } from "./";
 
-const Row = ({
-    index,
-    data,
-    tableManager,
-    measureRef
-}) => {
+const Row = ({ index, data, tableManager, measureRef }) => {
     const {
         config: { isVirtualScroll, rowIdField },
         rowEditApi: { editRow, getIsRowEditable },
@@ -17,30 +12,43 @@ const Row = ({
     } = tableManager;
 
     if (isVirtualScroll) {
-        if (index === 'virtual-start') {
-            return visibleColumns.map(visibleColumn =>
-                <div key={`${index}-${visibleColumn.id}`} style={{ minHeight: virtualItems[0]?.start }} />
-            );
+        if (index === "virtual-start") {
+            return visibleColumns.map((visibleColumn) => (
+                <div
+                    key={`${index}-${visibleColumn.id}`}
+                    style={{ minHeight: virtualItems[0]?.start }}
+                />
+            ));
         }
-        if (index === 'virtual-end') {
-            return visibleColumns.map(visibleColumn =>
-                <div key={`${index}-${visibleColumn.id}`} style={{ minHeight: totalSize - virtualItems[virtualItems.length - 1]?.end || 0 }} />
-            );
+        if (index === "virtual-end") {
+            return visibleColumns.map((visibleColumn) => (
+                <div
+                    key={`${index}-${visibleColumn.id}`}
+                    style={{
+                        minHeight:
+                            totalSize -
+                                virtualItems[virtualItems.length - 1]?.end || 0,
+                    }}
+                />
+            ));
         }
     }
 
-    let rowIndex = (index+1) + (pageRows.length * page - pageRows.length);
+    let rowIndex = index + 1 + (pageRows.length * page - pageRows.length);
     let rowId = data?.[rowIdField] || rowIndex;
     let disableSelection = !data || !getIsRowSelectable(data);
-    let isSelected = !!data && !!(selectedRowsIds.find(selectedRowId => selectedRowId === rowId));
-    let isEdit = !!data && editRow?.[rowIdField] === rowId && !!getIsRowEditable(data);
-    
-    return visibleColumns.map((visibleColumn, colIndex) =>
-        <CellContainer 
+    let isSelected =
+        !!data &&
+        !!selectedRowsIds.find((selectedRowId) => selectedRowId === rowId);
+    let isEdit =
+        !!data && editRow?.[rowIdField] === rowId && !!getIsRowEditable(data);
+
+    return visibleColumns.map((visibleColumn, colIndex) => (
+        <CellContainer
             key={`${visibleColumn.id}-${rowId}`}
             rowId={rowId}
-            data={rowId && (editRow?.[rowIdField] === rowId) ? editRow : data} 
-            rowIndex={rowIndex} 
+            data={rowId && editRow?.[rowIdField] === rowId ? editRow : data}
+            rowIndex={rowIndex}
             colIndex={colIndex}
             column={visibleColumn}
             isSelected={isSelected}
@@ -49,7 +57,7 @@ const Row = ({
             forwardRef={colIndex === 0 ? measureRef : undefined}
             tableManager={tableManager}
         />
-    )
+    ));
 };
 
 export default Row;
